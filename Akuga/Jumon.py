915ed4d6672e50ed831f684dd2820f8486260767
lyd1ng <lyding@protonmail.com>
2019-05-10 17:06:59 +0200

    Added an artifact class to jumon.py which represents an equipment and trap; Changed the jumon to handle its ability script execution by its own, Jumons can only have one equipment

--- Akuga/Jumon.py
+++ Akuga/Jumon.py
@@ -1,17 +1,45 @@
+class Artifact():
+    """
+    The abstraction around an equipment or artifact
+    """
+    def __init__(self, name, color, ability_script):
+        self.name = name
+        self.color = color
+        self.ability_script = ability_script
+
+    def run_ability_script(self):
+        """
+        Run the ability script of the artifact if its not None, which
+        it never should be
+        """
+        if self.ability_script is not None:
+            exec(self.ability_script)
+
+
 class Jumon():
     """
     The abstraction class around the Jumon,
     which is the unit to summon by a player
     """
     def __init__(self, name, color, base_level, movement,
-                 equipments, ability_script, owned_by):
+                 equipment, ability_script, owned_by):
         super().__init__()
         self.name = name
         self.color = color
         self.base_level = base_level
         self.level_offset = 0
         self.movement = movement
-        self.equipments = equipments
+        self.equipment = equipment
         self.ability_script = ability_script
         self.owned_by = owned_by
         self.blocking = False
+
+    def run_ability_script(self):
+        """
+        Invoke the ability script of the jumon and the ability
+        scripts of the equipment attached to the jumon
+        """
+        if self.ability_script is not None:
+            exec(self.ability_script)
+        if self.equipment is not None:
+            self.equipment.run_ability_script()
