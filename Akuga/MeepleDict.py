fe0610d0106b0ef1ade7f32aaa0dd1151f10a3e1
lyd1ng <lyding@protonmail.com>
2019-05-26 04:47:27 +0200

    Added a comment to the GetMeepleByName function

--- Akuga/MeepleDict.py
+++ Akuga/MeepleDict.py
@@ -1,5 +1,12 @@
 from Akuga.Meeple import (Jumon, TestNeutralJumon)
 
+"""
+This module contains a dictionary with all meeples in the game
+stored under their name. Every other part of the program will
+reference this meeples. This is used to translate the meeple names
+within the commands send over the internet to a the meeple references.
+"""
+
 MeepleDict = {
     "Jumon1\n": Jumon("Jumon1\n", "red", 400, 1, None, None),
     "Jumon2": Jumon("Jumon2", "blue", 400, 1, None, None),
@@ -11,4 +18,7 @@ MeepleDict = {
 
 
 def GetMeepleByName(meeple_name):
+    """
+    Get a meeple by its name
+    """
     return MeepleDict[meeple_name]
