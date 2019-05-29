3382958ab95e0c0d05a53521959d1243df128dcd
lyd1ng <lyding@protonmail.com>
2019-05-29 04:28:29 +0200

    Add a GetNeutralMeeples and GetNotNeutralMeeples function to get a pool of neutral or non neutral meeples from the meeple dict; Change the name of the neutral jumon accordingly to the rule defined within GetNeutralMeeples. Every neutral meeple has to have a name including __neutral; Add a very simple test snippet

--- Akuga/MeepleDict.py
+++ Akuga/MeepleDict.py
@@ -1,3 +1,4 @@
+import random
 from Akuga.Meeple import (Jumon, TestNeutralJumon)
 
 """
@@ -13,7 +14,7 @@ MeepleDict = {
     "Jumon3": Jumon("Jumon3", "green", 400, 1, None, None),
     "Jumon4": Jumon("Jumon4", "black", 400, 1, None, None),
     "Jumon5": Jumon("Jumon5", "white", 400, 1, None, None),
-    "NeutralJumon1": TestNeutralJumon("NeutralJumon1", None)
+    "NJ1__neutral": TestNeutralJumon("NeutralJumon1", None)
 }
 
 
@@ -22,3 +23,50 @@ def GetMeepleByName(meeple_name):
     Get a meeple by its name
     """
     return MeepleDict[meeple_name]
+
+
+def GetNeutralMeeples(amount):
+    """
+    Get a list of $amount neutral jumons without double occupancies
+    """
+    neutral_meeples = []
+    pool = []
+    # Filter for all neutral jumons (all jumons with a name including __neutral)
+    for neutral_meeple in filter(lambda x: x.find("__neutral") > 1,
+            list(MeepleDict.keys())):
+        neutral_meeples.append(neutral_meeple)
+    # Get random jumons from the list without double occupancies
+    indices_cached = []
+    for i in range(0, min(amount, len(neutral_meeples))):
+        random_index = random.randint(0, len(neutral_meeples) - 1)
+        while random_index in indices_cached:
+            random_index = random.randint(0, len(neutral_meeples) - 1)
+        pool.append(neutral_meeples[random_index])
+        indices_cached.append(random_index)
+    return pool
+
+
+def GetNotNeutralMeeples(amount):
+    """
+    Get a list of $amount not neutral jumons without double occupancies
+    """
+    not_neutral_meeples = []
+    pool = []
+    # Filter for all non neutral jumons (all jumons with a name not including __neutral)
+    for not_neutral_meeple in filter(lambda x: x.find("__neutral") < 0,
+            list(MeepleDict.keys())):
+        not_neutral_meeples.append(not_neutral_meeple)
+    # Get random jumons from the list
+    indices_cached = []
+    for i in range(0, min(amount, len(not_neutral_meeples))):
+        random_index = random.randint(0, len(not_neutral_meeples) - 1)
+        while random_index in indices_cached:
+            random_index = random.randint(0, len(not_neutral_meeples) - 1)
+        pool.append(not_neutral_meeples[random_index])
+        indices_cached.append(random_index)
+    return pool
+
+
+if __name__ == "__main__":
+    print(GetNotNeutralMeeples(10))
+    print(GetNeutralMeeples(10))
