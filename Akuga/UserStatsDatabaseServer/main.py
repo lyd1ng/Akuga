c3a207c5a1d02eb72bf1bc63975d5a050b1a04dc
lyd1ng <lyding@protonmail.com>
2019-06-04 04:50:31 +0200

    Fix the AddWin and AddLoos functions. Add a GetStats function to query the stats of a user in a game mode between and including two dates. Extend the whitelist by '-' as it is needed to send dates. Add a function today which uses datetime and strftime to get the current date as a string in the american format. Rewrite the handle_client function uses the network protokoll now to receive a packet and turn it into tokens. It also invokes the correct functions to handle the specified commands. This way only certain operations are allowed on the database.

--- Akuga/UserStatsDatabaseServer/main.py
+++ Akuga/UserStatsDatabaseServer/main.py
@@ -12,18 +12,25 @@ logging.basicConfig(filename='UserStatsDatabaseServer.log', level=logging.INFO)
 logger = logging.getLogger(__name__)
 running = True
 
+# TODO: wrap the sql output inside the akuga network protocoll
+
+def today():
+    """
+    Get the current date in the format %Y-%m-%-d
+    """
+    return datetime.today().strftime("%Y-%m-%d")
+
 
 def secure_string(string):
     """
     Return True if all characters in the string are part of the
     whitelist. Otherwise return False
     """
-    whitelist = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    whitelist = '-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
     for s in string:
         if s not in whitelist:
             return False
     return True
-
 
 
 def handle_sigint(sig, frame):
@@ -41,11 +48,12 @@ def InitDaylyEntry(cmd_queue, username, game_mode):
     for the user named username in the specified game mode.
     The date is always today to avoid tampering entries from the past
     """
-    command = '''insert into userstats(name, mode, date, wins, looses)
+    command = '''insert into userstats (name, mode, date, wins, looses)
         select '{0}', '{1}', '{2}', 0, 0
-        where not exists(select 1 from userstats where name='{0}
-        and mode='{1} and date='{2}'''.format(username, game_mode,
-            datetime.today)
+        where not exists(select 1 from userstats where name='{0}'
+        and mode='{1}' and date='{2}') '''.format(username, game_mode,
+            today())
+    print(command)
     logging.info("Enqueued command from: local")
     cmd_queue.put((None, "local", command))
 
@@ -65,8 +73,8 @@ def AddWin(connection, client_address, cmd_queue, username, game_mode):
     # the update command
     InitDaylyEntry(cmd_queue, username, game_mode)
     # Create the update command
-    command = '''update userstat set wins=wins+1 where name='{0}' and mode=\
-        '{1}' and date='{2}' '''.format(username, game_mode, datetime.today)
+    command = '''update userstats set wins=wins+1 where name='{0}' and mode=\
+        '{1}' and date='{2}' '''.format(username, game_mode, today())
     logger.info('Enqueue command from: ' + str(client_address))
     cmd_queue.put((connection, client_address, command))
     return 0
@@ -87,16 +95,76 @@ def AddLoose(connection, client_address, cmd_queue, username, game_mode):
     # the update command
     InitDaylyEntry(cmd_queue, username, game_mode)
     # Create the update command
-    command = '''update userstat set looses=looses+1 where name='{0}' and mode=\
-        '{1}' and date='{2}' '''.format(username, game_mode, datetime.today)
+    command = '''update userstats set looses=looses+1 where name='{0}' and mode=\
+        '{1}' and date='{2}' '''.format(username, game_mode, today())
     logger.info('Enqueue command from: ' + str(client_address))
     cmd_queue.put((connection, client_address, command))
     return 0
 
 
-# TODO: Write a new handle client which reads packets, parses the prot. and invoces the correct functions
+def GetStats(connection, client_address, cmd_queue, username,
+        game_mode, from_date, to_date):
+    """
+    Queries all the user stats of a user in a certain game mode between and
+    including the from and the to date
+    """
+    # if on of the parameters are insecure log it and return
+    if secure_string(username + game_mode + from_date + to_date) is False:
+        logger.info("One of the parameters where insecure!")
+        logger.info("Received from: " + str(client_address))
+        return -1
+    # Create the query command
+    command = '''select date,wins,looses from userstats where name='{0}' and
+        mode='{1}' and date >= '{2}' and date <= '{3}'
+        '''.format(username, game_mode, from_date, to_date)
+    print(command)
+    logger.info('Enqueue command from: ' + str(client_address))
+    cmd_queue.put((connection, client_address, command))
+    return 0
+
+
 def handle_client(connection, client_address, cmd_queue):
-    pass
+    while True:
+        try:
+            packet = connection.recv(512).decode('utf-8')
+        except socket.error:
+            connection.close()
+            break
+        if packet.find('END') < 0:
+            """
+            Simply discard the packet if there is not terminator found
+            """
+            continue
+        # Split the packet into tokens with the ":" delim
+        tokens = packet.split(":")
+        if tokens[0] == "ADD_WIN" and len(tokens) >= 3:
+            """
+            If the command token is ADD_WIN and enough tokens are received
+            add a win to the stats of a user in a certain game mode
+            """
+            username = tokens[1]
+            game_mode = tokens[2]
+            AddWin(connection, client_address, cmd_queue, username, game_mode)
+        if tokens[0] == "ADD_LOOSE" and len(tokens) >= 3:
+            """
+            If the command token is ADD_LOOSE and enough tokens are received
+            add a loose to the stats of a user in a certain game mode
+            """
+            username = tokens[1]
+            game_mode = tokens[2]
+            AddLoose(connection, client_address, cmd_queue, username, game_mode)
+        if tokens[0] == "GET_STATS" and len(tokens) >= 5:
+            """
+            If the command token is get_stats and enough tokens are received
+            request the stats of a user in a certain game mode from a start
+            to an end date
+            """
+            username = tokens[1]
+            game_mode = tokens[2]
+            from_date = tokens[3]
+            to_date = tokens[4]
+            GetStats(connection, client_address, cmd_queue, username,
+                game_mode, from_date, to_date)
 
 
 def sql_worker(cmd_queue):
@@ -130,9 +198,10 @@ def sql_worker(cmd_queue):
         except sqlite3.Error as e:
             logger.info("SQL Error from: " + str(client_address))
             result = e.args[0]
-        result = result.encode('utf-8')
-        connection.send(result)
-        logger.info("Send result to: " + str(client_address))
+        if connection is not None:
+            result = result.encode('utf-8')
+            connection.send(result)
+            logger.info("Send result to: " + str(client_address))
         cmd_queue.task_done()
         # Commit to the database to make the changes visible
         database.commit()
