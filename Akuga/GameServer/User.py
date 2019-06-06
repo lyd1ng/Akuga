15705f8ea5f4ecaf79d77d4e3b8f2502aec560f7
lyd1ng <lyding@protonmail.com>
2019-06-06 23:04:29 +0200

    Add a client address to user and remove the stats from user

--- Akuga/GameServer/User.py
+++ Akuga/GameServer/User.py
@@ -4,20 +4,17 @@ class User:
     play Akuga
     name: name of the player
     pass_hash: the md5 hash of its password
-    stats: a dictionary with the game mode as the key and wins and looses
-           stored as a tupel as the value
-           eg.: {'LastManStanding': (wins, looses)}
     connection: the connection socket
     """
-    def __init__(self, name, pass_hash, stats, connection):
+    def __init__(self, name, pass_hash, connection, client_address):
         self.name = name
         self.pass_hash = pass_hash
-        self.stats = stats
         self.connection = connection
+        self.client_address = client_address
 
     def __str__(self):
         """
         Returns the user as a string
         """
         return "User: " + self.name + " " + self.pass_hash\
-            + " " + str(self.connection)
+            + " " + str(self.client_address)
