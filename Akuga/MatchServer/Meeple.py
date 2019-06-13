6538817bf0a28f0728f5d774ab77bebb28d20844
lyd1ng <lyding@protonmail.com>
2019-06-13 23:42:15 +0200

    Split the base_level into attack and defense values, remove the base_level_offset values, it will be reimplemented later; Remove unneded test jumons and test artefacts

--- Akuga/MatchServer/Meeple.py
+++ Akuga/MatchServer/Meeple.py
@@ -50,13 +50,13 @@ class Jumon():
     The abstraction class around the Jumon,
     which is the unit to summon by a player
     """
-    def __init__(self, name, color, base_level, movement,
+    def __init__(self, name, color, attack, defense, movement,
                  equipment, owned_by):
         super().__init__()
         self.name = name
         self.color = color
-        self.base_level = base_level
-        self.level_offset = 0
+        self.attack = attack
+        self.defense = defense
         self.movement = movement
         self.equipment = equipment
         self.owned_by = owned_by
@@ -111,84 +111,9 @@ class Jumon():
         return (fsm.change_player_state, {})
 
 
-class TestArtefact(Artefact):
-    """
-    Change the name of the jumon
-    """
-    def __init__(self):
-        super().__init__("A1", False)
-        self.saved_jumon_name = ""
-
-    def attach_to(self, jumon):
-        self.saved_jumon_name = jumon.name
-        jumon.name += " A1"
-        jumon.level_offset += 150
-        super().attach_to(jumon)
-
-    def detach_from(self, jumon):
-        jumon.name = self.saved_jumon_name
-        jumon.level_offset -= 150
-        super().detach_from(jumon)
-
-    def special_ability(self, jumon, current_state, next_state_and_variables):
-        print(jumon.name + " is active in state " + current_state.name)
-        return next_state_and_variables
-
-
-class Test2Artefact(Artefact):
-    """
-    Change the name of the jumon
-    """
-    def __init__(self):
-        super().__init__("A2", False)
-        self.saved_jumon_name = ""
-
-    def attach_to(self, jumon):
-        self.saved_jumon_name = jumon.name
-        jumon.name += " equiped2"
-        super().attach_to(jumon)
-
-    def detach_from(self, jumon):
-        jumon.name = self.saved_jumon_name
-        super().detach_from(jumon)
-
-
-class TestJumon(Jumon):
-    def __init__(self, color, base_level, movement,
-                 equipment, owned_by):
-        super().__init__("TestJumon", color, base_level, movement, equipment, owned_by)
-
-    def special_ability(self, current_state, next_state_and_variables):
-        """
-        The very basic ability script which doesnt do anything
-        just returns the next_state_and_variables tuple
-        state_and_variables: [next_state_to_jump_to, variables_to_pass]
-        """
-        if current_state is current_state.fsm.summon_state:
-            next_state_and_variables[1]["summon_position"] = Position(0, 0)
-        return next_state_and_variables
-
-
-class Test2Jumon(Jumon):
-    def __init__(self, color, base_level, movement,
-                 equipment, owned_by):
-        super().__init__("Test2Jumon", color, base_level, movement, equipment, owned_by)
-
-    def special_ability(self, current_state, next_state_and_variables):
-        """
-        Try a state change if this jumon is summoned so the player has
-        a second turn
-        """
-        if current_state is current_state.fsm.summon_check_state\
-                or current_state is current_state.fsm.one_tile_battle_aftermath_state:
-            print("Jump to idle state for an extra turn")
-            return (current_state.fsm.idle_state, {})
-        return next_state_and_variables
-
-
 class TestNeutralJumon(Jumon):
     def __init__(self, name, owned_by):
-        super().__init__(name, "red", 300, 2, None, owned_by)
+        super().__init__(name, "red", 300, 300, 2, None, owned_by)
 
     def wrap(self, x, min_value, max_value):
         """
