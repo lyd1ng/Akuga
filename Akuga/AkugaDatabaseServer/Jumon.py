9b351c4caadca0515b8a4a111a285d1a9019ede4
lyd1ng <lyding@protonmail.com>
2019-07-14 18:14:05 +0200

    Add a Jumon.py module which contains all jumon database related code

---
+++ Akuga/AkugaDatabaseServer/Jumon.py
@@ -0,0 +1,40 @@
+import logging
+from Akuga.AkugaDatabaseServer.Network import (
+    secure_string,
+    send_packet)
+
+# Get the logger
+logger = logging.getLogger('AkugaDatabaseServer.Jumons')
+
+
+def get_all_jumon_names(connection, client_address, cmd_queue):
+    """
+    Query the database for the names of all jumons
+    """
+    command = ("select name from jumons", ())
+    logger.info("Enqueue 'get_all_jumon_names' from: " + str(client_address))
+    cmd_queue.put((connection, client_address, command))
+
+
+def get_all_basic_jumon_names(connection, client_address, cmd_queue):
+    """
+    Query the database for the name of all basic jumons
+    aka all jumons a user starts with
+    """
+    command = ("select name from jumons where basic=1", ())
+    logger.info("Enqueue 'get_all_basic_jumon_names' from: "
+        + str(client_address))
+    cmd_queue.put((connection, client_address, command))
+
+
+def get_jumon_by_name(connection, client_address, cmd_queue, jumon_name):
+    """
+    Query the database for a jumon by its name
+    """
+    if secure_string(jumon_name) is False:
+        send_packet(connection, ['ERROR', 'Insecure Parameter'])
+        return
+    command = ("select * from jumons where name=?", (jumon_name, ))
+    logger.info("Enqueue 'get_jumon_by_name' from: "
+        + str(client_address))
+    cmd_queue.put((connection, client_address, command))
