01c6aaf5056ee6b812467e50b5368f19a3e477a2
lyd1ng <lyding@protonmail.com>
2019-07-17 22:44:05 +0200

    Add the START_CREDITS definition, this is used in the register user command

--- Akuga/GameServer/GlobalDefinitions.py
+++ Akuga/GameServer/GlobalDefinitions.py
@@ -1,3 +1,4 @@
+# Network related definitions
 SERVER_ADDRESS = ('127.0.0.1', 10315)
 USER_DBS_ADDRESS = ('127.0.0.1', 10098)
 MAX_ACTIVE_CONNECTIONS = 1024
@@ -5,3 +6,6 @@ EMAIL_SERVER = 'mail.gmx.net'
 EMAIL_PORT = 587
 EMAIL_ADDRESS = "Akuga@gmx.de"
 EMAIL_PASSWORD = "akugathemythicmachiene"
+
+# User related definitions
+START_CREDITS = 444
