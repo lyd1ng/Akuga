325c16ddae5ec58ecad4a3551802c2723117499b
lyd1ng <lyding@protonmail.com>
2019-07-21 01:17:04 +0200

    Change the jumon set module to work on dictionaries

--- Akuga/JumonSet.py
+++ Akuga/JumonSet.py
@@ -9,6 +9,8 @@ def jumon_set_from_list(jumon_name_list):
     Creates a dictionary of the form jumon_name: amount
     from a list of jumon names
     """
+    if jumon_name_list == '':
+        return {}
     jumon_set = {}
     for name in jumon_name_list:
         try:
@@ -20,6 +22,21 @@ def jumon_set_from_list(jumon_name_list):
             # set add one with an amount of 1
             jumon_set[name] = 1
     return jumon_set
+
+
+def insert_name(jumon_set, jumon_name):
+    """
+    Insert a jumon name to a jumon_set,
+    that means add a new record if there is no record
+    for the jumon or increment the amount of the specified record
+    """
+    try:
+        # Just try to increment the amount of the record
+        jumon_set[jumon_name] += 1
+    except KeyError:
+        # If there is no record for this name a KeyError is thrown
+        # add a new record with an amount of 1
+        jumon_set[jumon_name] = 1
 
 
 def is_subset(smaller_set, larger_set):
@@ -48,9 +65,20 @@ def is_subset(smaller_set, larger_set):
     return True
 
 
+def serialize_set(jumon_set):
+    """
+    Return the set as a plain string of coma delimited jumon names.
+    The amount of a jumos is representes by repetitions of the name
+    """
+    serialized_string = ''
+    for record in jumon_set.items():
+        for i in range(record[1]):
+            serialized_string += record[0] + ','
+    # Skip the last character its a superfluous coma
+    return serialized_string[:-1]
+
+
 if __name__ == '__main__':
     setA = jumon_set_from_list(['jumon1', 'jumon2', 'jumon2', 'jumon4'])
     setB = jumon_set_from_list(['jumon1', 'jumon2', 'jumon2', 'jumon4'])
-    print(setA)
-    print(setB)
-    print(is_subset(setA, setB))
+    print(serialize_set(setA))
