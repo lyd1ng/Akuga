a629e349d8d4822266d2e4f7ee65a456c0f83119
lyd1ng <lyding@protonmail.com>
2019-07-30 17:33:20 +0200

    Change the event definitions to be just integers, this allows a wider range of integers, so the events can be numbered in intervalls of 100

--- Akuga/EventDefinitions.py
+++ Akuga/EventDefinitions.py
@@ -1,21 +1,22 @@
-import pygame
 """
-This file contains every user defined events
+This file contains every user defined event,
+0 - 99: Input events for the rule building fsm
+100 - 199: Output events from the rule building fsm
+200 - 299: Network related events
 """
 
 # Events to leave the idle state
-SUMMON_JUMON_EVENT = pygame.USEREVENT + 1
-SELECT_JUMON_TO_MOVE_EVENT = pygame.USEREVENT + 2
-SELECT_JUMON_TO_SPECIAL_MOVE_EVENT = pygame.USEREVENT + 3
-PICK_JUMON_EVENT = pygame.USEREVENT + 4
+SUMMON_JUMON_EVENT = 1
+SELECT_JUMON_TO_MOVE_EVENT = 2
+SELECT_JUMON_TO_SPECIAL_MOVE_EVENT = 3
+PICK_JUMON_EVENT = 4
+TIMEOUT_EVENT = 5
+
 
 # Events thrown by the state machiene
-PLAYER_HAS_WON = pygame.USEREVENT + 5
-MATCH_IS_DRAWN = pygame.USEREVENT + 6
-TURN_ENDS = pygame.USEREVENT + 7
+PLAYER_HAS_WON = 100
+MATCH_IS_DRAWN = 101
+TURN_ENDS = 102
 
 # Network Protocoll Events
-PACKET_PARSER_ERROR_EVENT = pygame.USEREVENT + 8
-
-# Serverside events
-TIMEOUT_EVENT = pygame.USEREVENT + 9
+PACKET_PARSER_ERROR_EVENT = 200
