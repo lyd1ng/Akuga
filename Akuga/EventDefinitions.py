471965431513053fa7a3f04d52411986c84eb2ef
lyd1ng <lyding@protonmail.com>
2019-07-30 18:05:10 +0200

    Add a Event class which mimics the Event class of the pygame library

--- Akuga/EventDefinitions.py
+++ Akuga/EventDefinitions.py
@@ -5,6 +5,10 @@ This file contains every user defined event,
 200 - 299: Network related events
 """
 
+# An empty event which is thrown if no event is in the queue
+# this allows a non blocking get on the queue
+NOEVENT = -1
+
 # Events to leave the idle state
 SUMMON_JUMON_EVENT = 1
 SELECT_JUMON_TO_MOVE_EVENT = 2
@@ -20,3 +24,19 @@ TURN_ENDS = 102
 
 # Network Protocoll Events
 PACKET_PARSER_ERROR_EVENT = 200
+
+
+class Event:
+    def __init__(self, _type, **kwargs):
+        self.type = _type
+        # Make every element of the kwarg dictionarie a member of this
+        # event instance. So e = Event(0, a='a', b='b'); print(e.a)
+        # is possible
+        self.__dict__.update(kwargs)
+
+    def __eq__(self, other):
+        '''
+        Allows an equality check between an instance of an Event and an integer
+        Only the event type is compared
+        '''
+        return self.type == other
