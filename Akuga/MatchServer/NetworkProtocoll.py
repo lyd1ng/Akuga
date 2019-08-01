33e201cccb5555e7379e4c1ffd62ce34db8f2113
lyd1ng <lyding@protonmail.com>
2019-08-01 03:07:39 +0200

    Remove the MessagePropagator module and add its function to the NetworkProtocoll module

--- Akuga/MatchServer/NetworkProtocoll.py
+++ Akuga/MatchServer/NetworkProtocoll.py
@@ -60,6 +60,28 @@ def send_packet(connection, tokens, terminator="END\n"):
     connection.send(query)
 
 
+def propagate_message(message_event):
+    '''
+    Use the send_packet function to send a packet made out of tokens to all
+    users specified in the message_event.
+    Therefor the message_event has to provide:
+    users: a list of user instances
+    tokens: a list of tokens building the packet
+
+    Brokene connections will be ignored as timeout and reconnects are
+    handeld elsewhere in the code
+    '''
+    users = message_event.users
+    tokens = message_event.tokens
+    for user in users:
+        try:
+            send_packet(user.connection, tokens)
+        except IOError:
+            # If the connection is broken just do nothing it will be
+            # handeld elsewhere in the code
+            pass
+
+
 def handle_match_connection(tokens, queue, jumons_in_play):
     """
     Receives a package from connection with a timeout of the defined
