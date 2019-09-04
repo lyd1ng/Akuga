c85327ba05c06dac0b7afe68d13fe41635130537
lyd1ng <lyding@protonmail.com>
2019-09-04 23:07:41 +0200

    Remove the secure string and weak_secure string checks as the sqlite escape functionality is used everywhere

--- Akuga/AkugaDatabaseServer/UserStats.py
+++ Akuga/AkugaDatabaseServer/UserStats.py
@@ -27,7 +27,6 @@ def init_dayly_entry(cmd_queue, username, game_mode):
         where not exists(select 1 from userstats where name=:name
         and mode=:mode and date=:date)''',
         {'name': username, 'mode': game_mode, 'date': today()})
-    print(command)
     logger.info("Enqueued command from: local\n")
     cmd_queue.put((None, "local", command))
 
@@ -37,12 +36,6 @@ def add_win(connection, client_address, cmd_queue, username, game_mode):
     Adds a win for the user username in the game mode game_mode
     the date is always today to avoid tempering data in the past
     """
-    # If on of the parameters are insecure log it and return
-    if secure_string(username) is False or secure_string(game_mode) is False:
-        logger.info("Username or gamemode where insecure!\n")
-        logger.info("Received from: " + str(client_address))
-        send_packet(connection, ["ERROR", "Insecure Parameter"])
-        return -1
     # Enqueues to create an empty stats field if none exists
     # Cause the queue is a fifo structure this will be executed before
     # the update command
@@ -60,12 +53,6 @@ def add_loose(connection, client_address, cmd_queue, username, game_mode):
     Adds a loose for the user username in the game mode game_mode
     the date is always today to avoid tempering data in the past
     """
-    # If on of the parameters are insecure log it and return
-    if secure_string(username) is False or secure_string(game_mode) is False:
-        logger.info("Username or gamemode where insecure!\n")
-        logger.info("Received from: " + str(client_address))
-        send_packet(connection, ["ERROR", "Insecure Parameter"])
-        return -1
     # Enqueues to create an empty stats field if none exists
     # Cause the queue is a fifo structure this will be executed before
     # the update command
@@ -84,13 +71,6 @@ def get_stats(connection, client_address, cmd_queue, username, game_mode,
     Queries all the user stats of a user in a certain game mode between and
     including the from and the to date
     """
-    # if on of the parameters are insecure log it and return
-    if secure_string(username + game_mode + from_year + from_month + from_day
-            + to_year + to_month + to_day) is False:
-        logger.info("One of the parameters where insecure!\n")
-        logger.info("Received from: " + str(client_address))
-        send_packet(connection, ["ERROR", "Insecure Parameter"])
-        return -1
     # Create the date strings
     from_date = from_year + '-' + from_month + '-' + from_day
     to_date = to_year + '-' + to_month + '-' + to_day
@@ -98,7 +78,6 @@ def get_stats(connection, client_address, cmd_queue, username, game_mode,
     command = ('''select date,wins,looses from userstats where name=? and
         mode=? and date >= ? and date <= ?''',
         (username, game_mode, from_date, to_date))
-    print(command)
     logger.info('Enqueue command from: ' + str(client_address))
     cmd_queue.put((connection, client_address, command))
     return 0
