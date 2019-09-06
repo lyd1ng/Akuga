7f89da8f4998c284c90bd84fee6d0c91248f18a2
lyd1ng <lyding@protonmail.com>
2019-09-06 03:39:48 +0200

    Add a receive line function which safely receives a complete line from the wire; Add functions to the new class StreamSocketCommunicator

--- Akuga/GameServer/Network.py
+++ Akuga/GameServer/Network.py
@@ -2,69 +2,65 @@
 This module contains all functions related to network communication
 """
 import socket
-from ast import literal_eval
-
-whitelist = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+from json import (loads, dumps, JSONDecodeError)
 
 
-def recv_packet(connection, nbytes, delimiter=":", terminator="END\n"):
-    """
-    Receive a packet and convert it into tokens using the delimiter.
-    Return ['ERROR', $msg] if there no terminator is found,
-    return all tokens except the terminator token otherwise.
-    If the connection was closed raise a socket.error
-    """
-    packet = connection.recv(nbytes)
-    if not packet:
-        raise socket.error
-    packet = packet.decode('utf-8')
-    tokens = packet.split(delimiter)
-    if tokens[-1] == terminator:
-        return tokens[0:-1]
-    return ['ERROR', 'No terminator found while recv the packet']
+class StreamSocketCommunicator:
+    def __init__(self, connection, nbytes):
+        '''
+        connection: The stream socket to work on
+        nbytes: Max bytes to read once at a time
+        '''
+        self.connection = connection
+        self.nbytes = nbytes
 
+    def receive_line(self):
+        '''
+        Receives a complete line from a stream socket.
+        Therefor the stream of bytes have to be cached and scaned for
+        terminator signs. The newline character in this case
+        '''
+        index = self.cached_string.find('\n')
+        while index < 0:
+            # As long as there is no complete line cached receive bytes
+            # from the wire
+            new_data = self.connection.recv(self.nbytes).decode('utf-8')
+            if not new_data:
+                # If the connection was closed by the foreign host
+                # raise a socket error
+                raise socket.error
+            self.cached_string += new_data
+            # Again look for the newline character
+            index = self.cached_string.find('\n')
+        # Now a complete line is cached so return it and remove it from
+        # the cache
+        complete_line = self.cached_string[:index]
+        self.cached_string = self.cached_string[index + 1:]
+        return complete_line
 
-def send_packet(connection, tokens, terminator="END\n"):
-    """
-    Send a packet containing multiple tokens.
-    Every token is converted to a string using the str function
-    for better convenients and is encoded using utf-8 encoding.
-    A packet has the form token1:token2:...:tokenN:terminator
-    """
-    query = ""
-    for t in tokens:
-        query += str(t) + ":"
-    if terminator is not None:
-        query += str(terminator)
-    query = query.encode('utf-8')
-    connection.send(query)
+    def recv_packet(self):
+        """
+        Receive a packet, aka a complete line, from the wire and use json
+        to deserialize it. Only lists are accepted as valid datatypes
+        """
+        json_line = self.receive_line()
+        try:
+            packet = loads(json_line)
+        except JSONDecodeError:
+            self.send_packet(['ERROR', 'Invalid Packet'])
+            return None
+        if type(packet) is not list:
+            self.send_packet(['ERROR', 'Invalid Packet'])
+            return None
+        return packet
 
-
-def secure_string(string):
-    """
-    Return True if all characters in the string are part of the
-    whitelist. Otherwise return False
-    """
-    for s in string:
-        if s not in whitelist:
-            return False
-    return True
-
-
-def receive_dbs_response(userdbs_connection, nbytes):
-    """
-    Receive up to 512 bytes from the database server
-    and parse it using the parse_literal function of the
-    ast module. This can be done as the pysqlite module
-    return its results as a python list
-    """
-    tokens = recv_packet(userdbs_connection, nbytes)
-    # If an error is received return None and the error msg received
-    if tokens[0] == "ERROR":
-        return (None, tokens[1])
-    # If no error is received return the parsed literal and "" as msg
-    print("Litetal to parse: " + tokens[1])
-    return (literal_eval(tokens[1]), "")
+    def send_packet(self, tokens):
+        """
+        Send a python list by serialising it using json
+        """
+        json_dump = dumps(tokens) + '\n'
+        json_dump = json_dump.encode('utf-8')
+        self.connection.send(json_dump)
 
 
 # TODO: Doesnt work yet, authentication fails...
