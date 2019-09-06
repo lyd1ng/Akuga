aca7670e5e4ceb7f1b24263c36595d1aefbbcc22
lyd1ng <lyding@protonmail.com>
2019-09-06 03:41:04 +0200

    Also use the StreamSocketCommunicator in the database server

--- Akuga/AkugaDatabaseServer/Network.py
+++ Akuga/AkugaDatabaseServer/Network.py
@@ -1,14 +1,71 @@
-def send_packet(connection, tokens, terminator="END\n"):
+"""
+This module contains all functions related to network communication
+"""
+import socket
+from json import (loads, dumps, JSONDecodeError)
+
+
+class StreamSocketCommunicator:
+    def __init__(self, connection, nbytes):
+        '''
+        connection: The stream socket to work on
+        nbytes: Max bytes to read once at a time
+        '''
+        self.connection = connection
+        self.nbytes = nbytes
+
+    def receive_line(self):
+        '''
+        Receives a complete line from a stream socket.
+        Therefor the stream of bytes have to be cached and scaned for
+        terminator signs. The newline character in this case
+        '''
+        index = self.cached_string.find('\n')
+        while index < 0:
+            # As long as there is no complete line cached receive bytes
+            # from the wire
+            new_data = self.connection.recv(self.nbytes).decode('utf-8')
+            if not new_data:
+                # If the connection was closed by the foreign host
+                # raise a socket error
+                raise socket.error
+            self.cached_string += new_data
+            # Again look for the newline character
+            index = self.cached_string.find('\n')
+        # Now a complete line is cached so return it and remove it from
+        # the cache
+        complete_line = self.cached_string[:index]
+        self.cached_string = self.cached_string[index + 1:]
+        return complete_line
+
+    def recv_packet(self):
+        """
+        Receive a packet, aka a complete line, from the wire and use json
+        to deserialize it. Only lists are accepted as valid datatypes
+        """
+        json_line = self.receive_line()
+        try:
+            packet = loads(json_line)
+        except JSONDecodeError:
+            self.send_packet(['ERROR', 'Invalid Packet'])
+            return None
+        if type(packet) is not list:
+            self.send_packet(['ERROR', 'Invalid Packet'])
+            return None
+        return packet
+
+    def send_packet(self, tokens):
+        """
+        Send a python list by serialising it using json
+        """
+        json_dump = dumps(tokens) + '\n'
+        json_dump = json_dump.encode('utf-8')
+        self.connection.send(json_dump)
+
+
+# TODO: Doesnt work yet, authentication fails...
+def send_password_to_client_email(username, password, user_email):
     """
-    Send a packet containing multiple tokens.
-    Every token is converted to a string using the str function
-    for better convenients and is encoded using utf-8 encoding.
-    A packet has the form token1:token2:...:tokenN:terminator
+    Send the password to the client via email
     """
-    query = ""
-    for t in tokens:
-        query += str(t) + ":"
-    if terminator is not None:
-        query += str(terminator)
-    query = query.encode('utf-8')
-    connection.send(query)
+    pass
